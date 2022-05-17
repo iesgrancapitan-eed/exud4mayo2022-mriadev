a5509fedb9e88e01c49562ec6aa1b0d7825ee81b
mriadev <mcervillaalcalde@gmail.com>
2022-05-17 09:11:41 +0200

    refactor: Mascota

---
+++ src/com/mca/examen/OptimizacionYDocumentacion/Mascota.java
@@ -0,0 +1,15 @@
+package com.mca.examen.OptimizacionYDocumentacion;
+
+public class Mascota {
+
+	protected String name;
+
+	public Mascota() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
