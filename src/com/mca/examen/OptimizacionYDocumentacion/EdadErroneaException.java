b322244c72b62ad15d77069a710c357647a92bec
mriadev <mcervillaalcalde@gmail.com>
2022-05-17 09:04:02 +0200

    refactor: nombre del paquete

---
+++ src/com/mca/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,8 @@
+package com.mca.examen.OptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
---
+++ src/com/mca/examen/OptimizacionYDocumentacion/package-info.java
@@ -0,0 +1,1 @@
+package com.mca.examen.OptimizacionYDocumentacion;
